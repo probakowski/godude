e9d78b92c1edbffc8fd3824c967a73e348c7eb3a
Przemko Robakowski <przemko.robakowski@goteleport.com>
2026-07-24 01:18:27 +0200

    Workflow

---
+++ a.ps1
@@ -0,0 +1,5 @@
+$VSINSTALLDIR = $(vswhere.exe -latest -requires Microsoft.VisualStudio.Component.VC.Llvm.Clang -property installationPath)
+$VCINSTALLDIR = Join-Path $VSINSTALLDIR "VC"
+$LLVM_ROOT = Join-Path $VCINSTALLDIR "Tools\Llvm\x64"
+echo $LLVM_ROOT
+ls $LLVM_ROOT
